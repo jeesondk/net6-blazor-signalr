c20a277b39d00b889134372938eaed99365fa143
Jesper Jensen <jesper.jensen@keemail.me>
2022-01-15 16:06:40 +0100

    feat: Basic chat / signalR with Blazor

---
+++ Server/Hubs/ChatHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace net6_blazor_signalr.Server.Hubs;
+
+public class ChatHub: Hub
+{
+    public async Task SendMessage(string user, string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+}
